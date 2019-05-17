edf12a15028c15c29f1528e0d35374d490862f3d
Dhruv Sharma <dhruvsharma0503@gmail.com>
2019-05-17 02:17:02 -0700

    added src/ + base files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HodorText from './HodorText.js'
+import HodorForm from './HodorForm.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {val:0};
+    this.HodorTextElement = React.createRef();
+    this.update = this.update.bind(this);
+  }
+
+
+  componentDidMount(){
+    document.title = "Hodor"
+  }
+
+  update = () => {
+    this.HodorTextElement.current.update({val:0});
+  }
+
+  render(){
+    return(
+      <div className="App centered">
+        <HodorText ref={this.HodorTextElement}/>
+        <HodorForm callBack={this.update}/>
+      </div>
+    );
+  }
 }
 
 export default App;
